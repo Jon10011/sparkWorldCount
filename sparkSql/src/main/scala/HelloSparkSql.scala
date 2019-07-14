42291c58232455720f64deab0e6da32495200c22
Jon <dong_song1@163.com>
2019-07-14 20:13:55 +0800

    spark DSL SQL

--- sparkSql/src/main/scala/HelloSparkSql.scala
+++ sparkSql/src/main/scala/HelloSparkSql.scala
@@ -1,25 +1,28 @@
+import org.apache.spark
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 
 object HelloSparkSql {
   def main(args: Array[String]): Unit = {
     //获取conf
-    val conf = new SparkConf().setAppName("Jon").setMaster("local[*]")
+    val conf = new SparkConf().setAppName("Jon").setMaster("local[2]")
 
 //    //获取sc
     val sc = new SparkContext(conf)
 //
-//    //获取sparkSession
+    //获取sparkSession
 //    val spark = new SparkSession(sc)
+//
+//    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+//    val df = sqlContext.read.json("/Users/jon/Desktop/学习笔记/resourse/employees.json")
 
     val spark = SparkSession.builder().config(conf).getOrCreate()
 
-//    val df = spark.read.json()
-
     //生成DataFrame
-    val df = spark.read.json("/Users/jon/Desktop/学习笔记/resourse/people.json")
+    val df = spark.read.json("/Users/jon/Desktop/学习笔记/resourse/employees.json")
 
     //展示所有数据
+    df.printSchema()
     df.show()
 
     //DSL
@@ -32,7 +35,7 @@ object HelloSparkSql {
     spark.sql("select * from people").show
 
     //关闭
-    spark.close()
+    //spark.close()
     sc.stop()
   }
 }
