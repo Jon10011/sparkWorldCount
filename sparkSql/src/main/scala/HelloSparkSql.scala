b2a87b5c91de0647d4199c0f1b39c847ef46fa33
Jon <dong_song1@163.com>
2019-07-14 17:56:17 +0800

    spark DSL SQL

--- sparkSql/src/main/scala/HelloSparkSql.scala
+++ sparkSql/src/main/scala/HelloSparkSql.scala
@@ -7,14 +7,14 @@ object HelloSparkSql {
     val conf = new SparkConf().setAppName("Jon").setMaster("local[*]")
 
 //    //获取sc
-//    val sc = new SparkContext(conf)
+    val sc = new SparkContext(conf)
 //
 //    //获取sparkSession
 //    val spark = new SparkSession(sc)
 
     val spark = SparkSession.builder().config(conf).getOrCreate()
 
-    val df = spark.read.json()
+//    val df = spark.read.json()
 
     //生成DataFrame
     val df = spark.read.json("/Users/jon/Desktop/学习笔记/resourse/people.json")
